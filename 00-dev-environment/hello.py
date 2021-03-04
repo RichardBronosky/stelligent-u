091a19a5182a32beea5317666ca41d7dadc68cca
Bruno Bronosky <bruno@bronosky.com>
2021-03-04 10:12:56 +0000

    Add python hello world
    - Used an Easter Egg
    - Used `main` pattern
    - Formatted with `yapf -i "$file"`
    - SCA with `pylint "$file"` (score 10/10)

---
+++ 00-dev-environment/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Lab 0.1.4 - Task: hello world"""
+
+
+def main():
+    """ FROM: https://rosettacode.org/wiki/Hello_world/Text#Python """
+    # pylint: disable=unused-import,import-outside-toplevel,import-error
+    import __hello__
+
+
+if __name__ == '__main__':
+    main()
