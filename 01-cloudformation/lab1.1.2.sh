ca2d00f3fd699136384d21dae28cf631cb63cc6b
Bruno Bronosky <bruno@bronosky.com>
2021-03-09 00:20:13 -0600

    Add formatting to make future commits more readable

--- 01-cloudformation/lab1.1.2.sh
+++ 01-cloudformation/lab1.1.2.sh
@@ -1,1 +1,5 @@
-aws cloudformation create-stack --stack-name StelligentULab1n1n1uBrunoBronosky --template-body file://cfn-s3_example.yaml
+#!/bin/bash
+
+aws cloudformation create-stack \
+        --stack-name StelligentULab1n1n1uBrunoBronosky \
+        --template-body file://cfn-s3_example.yaml
